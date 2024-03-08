414dfad7c94bb9541f2425eb99f8ffcee4d930f9
Piotr Stawarski <piotr.stawarski@zdt.io>
2024-03-08 15:49:56 +0100

    outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "arn" {
+  description = "Full ARN of the repository."
+  value       = aws_ecr_repository.main.arn
+
+}
+
+output "registry_id" {
+  description = "The registry ID where the repository was created."
+  value       = aws_ecr_repository.main.registry_id
+}
+
+output "repository_url" {
+  description = "The URL of the repository."
+  value       = aws_ecr_repository.main.repository_url
+}
